6f9b488728f0816d3757c8f5a352737adf559c9b
iniwahyu <wrep17@gmail.com>
2019-03-11 12:51:48 +0700

    Config Tambahan

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -1,13 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-
-	public function __construct()
-	{
-		parent::__construct();
-		// Load Model
-	}
+class Auth_model extends CI_Model {
 
 	public function index()
 	{
