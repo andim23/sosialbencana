fab652a19f7e60f8b7c37bab5854cc30dbb9686e
fhmanwar <fhmianwar@gmail.com>
2019-04-16 00:53:00 +0700

    cek

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -4,7 +4,7 @@
 <nav class="navbar navbar-inverse ">
 	<div class="container">
     <!-- header -->
-    <div class="w3_agile_logo"><h1 class="text-left"><a href="index.html">candid</a></h1></div>
+    <div class="w3_agile_logo"><h1 class="text-left"><a href="<?php echo base_url() ?>"><?php echo SITE_NAME ?></a></h1></div>
     <!-- //header -->
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
@@ -15,8 +15,7 @@
 
   	<div class="collapse navbar-collapse top-nav w3l navbar-right" id="bs-example-navbar-collapse-1">
   		<ul class="nav navbar-nav linkEffects linkHoverEffect_11 custom-menu">
-  			<li class="agile_active"><a href="<?php echo base_url() ?>" class="scroll"><span>home</span></a></li>
-  			<li><a href="#about"><span>about us</span></a></li>
+  			<li class="agile_active"><a href="#about"><span>about us</span></a></li>
   			<li><a href="#gallery"><span>gallery </span></a></li>
   			<li><a href="#team" ><span>team</span></a></li>
         <li><a href="<?php echo base_url('home/contact') ?>"><span>Contact</span></a></li>
